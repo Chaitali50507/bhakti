5cb98086981377f2bdf3a1d6edc65679510467ee
Chaitali Bhaktha <Chaitali.Bhaktha@sapiens.com>
2020-12-19 15:26:03 -0700

    pushing changes

---
+++ src/main/java/RahulShettyAcademy/MavenJava/MobileTest.java
@@ -0,0 +1,23 @@
+package RahulShettyAcademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class MobileTest {
+	
+	
+	@Test
+	public void NativeAppAndroid(){
+		
+		System.out.println("NativeAppAndroid");
+		
+		
+	}
+	
+	@Test
+	public void IOSApp(){
+		
+		System.out.println("IOSApp");	
+		
+	}
+
+}
